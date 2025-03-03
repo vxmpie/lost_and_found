f23e14b1946dbc52a0b18d6d725aaeb38f1a281e
Wiwat Suwannasit <marple.rblx@gmail.com>
2025-03-03 10:53:53 +0700

    upd

--- back/src/database.py
+++ back/src/database.py
@@ -1,4 +1,3 @@
-import mysql.connector
 import os
 from mysql.connector import pooling
 from dotenv import load_dotenv
@@ -9,7 +8,9 @@ db_config = {
     "host": os.getenv("DB_HOST"),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASS"),
-    "database": os.getenv("DB_NAME")
+    "database": os.getenv("DB_NAME"),
+    "charset": "utf8mb4",
+    "collation": "utf8mb4_general_ci"
 }
 
 connection_pool = pooling.MySQLConnectionPool(pool_name="mypool", pool_size=10, **db_config)
