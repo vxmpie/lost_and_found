ed1b12303e50f02d86bd8d3733c230b970732943
Wiwat Suwannasit <marple.rblx@gmail.com>
2025-03-06 07:48:04 +0700

    upd

--- Back/config.php
+++ Back/config.php
@@ -1,8 +1,13 @@
 <?php
-define('DB_SERVER', 'sql313.infinityfree.com');
-define('DB_USERNAME', 'if0_38449771');
-define('DB_PASSWORD', 'MRt5NNkCKTO');
-define('DB_NAME', 'if0_38449771_foundify');
+// define('DB_SERVER', 'sql313.infinityfree.com');
+// define('DB_USERNAME', 'if0_38449771');
+// define('DB_PASSWORD', 'MRt5NNkCKTO');
+// define('DB_NAME', 'if0_38449771_foundify');
+
+define('DB_SERVER', '89.213.177.80');
+define('DB_USERNAME', 'toon');
+define('DB_PASSWORD', 'toon1234');
+define('DB_NAME', 'lostnfound');
 
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
